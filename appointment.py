4543f40e93df7b39102b62e4e64a74f102c764ac
Naman Chauhan <chauhannaman98@gmail.com>
2020-04-02 19:14:45 +0530

    Update changes to log windol

--- appointment.py
+++ appointment.py
@@ -79,9 +79,24 @@ class App:
         self.submit = Button(self.left, text="Add Appointment", width=20, height=2, bg='steelblue', command=self.add_appointment)
         self.submit.place(x=300, y=340)
 
+        # getting the number of appointments fixed to view in the log
+        sql2 = "SELECT ID FROM appointments "
+        self.result = c.execute(sql2)
+        for self.row in self.result:
+            self.id = self.row[0]
+            ids.append(self.id)
+
+        # ordering the ids
+        self.new = sorted(ids)
+        self.final_id = self.new[len(ids)-1]
+
         # displaying the logs in right frame
-        self.box = Text(self.right, width=40, height=38)
-        self.box.place(x=30, y=30)
+        self.logs = Label(self.right, text="Appointment Log", font=('arial 28 bold'), fg='white', bg='steelblue')
+        self.logs.place(x=20, y=0)
+
+        self.box = Text(self.right, width=50, height=40)
+        self.box.place(x=20, y=60)
+        self.box.insert(END, "Total Appointments till now :  " + str(self.final_id))
 
     # function to call when the submit button is clicked
     def add_appointment(self):
@@ -103,17 +118,8 @@ class App:
             conn.commit()
             tkinter.messagebox.showinfo("Success","Appointment for "+str(self.val1)+" has been created")
             
-            # getting the number of appointments fixed to view in the log
-            sql2 = "SELECT ID FROM appointment"
-            self.result = c.execute(sql2)
-            for self.row in self.result:
-                self.id = self.row[0]
-                ids.append(self.id)
 
-            # ordering the ids
-            self.new = sorted(ids)
-            self.final_id = self.new[len(ids)-1]
-            self.box.insert(END, "Appointment fixed"+str(self.final_id))
+            self.box.insert(END, '\nAppointment fixed for ' + str(self.val1) + ' at ' + str(self.val5))
 
 
 #creating the object
