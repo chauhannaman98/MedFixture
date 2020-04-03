7102032f14c5be7d2404f6c01c37d865ffb2e6f4
Naman Chauhan <chauhannaman98@gmail.com>
2020-04-03 19:34:39 +0530

    Add DB query

--- update.py
+++ update.py
@@ -16,16 +16,30 @@ c = conn.cursor()
 class App:
     def __init__(self, master):
         self.master = master
+        self.heading = Label(master, text="Update Appointments", fg='steelblue', font=('arial 40 bold'))
+        self.heading.place(x=150, y=0) 
 
-        self.name = Label(master, text="Pateint's name", font('arial 18 bold'))
+        # search criteria
+        self.name = Label(master, text="Enter Patient's Name", font=('arial 18 bold'))
         self.name.place(x=0, y=60)
 
+        # entry for the name
         self.namenet = Entry(master, width=30)
-        self.namenet.place(x=200, y=62)
+        self.namenet.place(x=300, y=62)
 
         # search button
-        self.search = Button(master, text="Search", width=17, height=1, bg='steelblue')
-        self.search
+        self.search = Button(master, text="Search", width=12, height=1, bg='steelblue', command=self.search_db)
+        self.search.place(x=350, y=102)
+
+    # function to search
+    def search_db(self):
+        self.input = self.namenet.get()
+        
+        # execute sql
+        sql = "SELECT * FROM appointments WHERE name LIKE ?"
+        self.res = c.execute(sql, (self.input))
+        for self.row in self.res:
+            print(self.row)
 
 #creating the object
 root = tk.Tk()
