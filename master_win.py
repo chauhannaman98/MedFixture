85437192fa7770c2a9b74c6e852fa4c72f0c67d6
Naman Chauhan <chauhannaman98@gmail.com>
2020-04-05 19:41:24 +0530

    Create master window

---
+++ master_win.py
@@ -0,0 +1,12 @@
+# import modules
+try:
+    # for Python2
+    from Tkinter import *   ## notice capitalized T in Tkinter 
+except ImportError:
+    # for Python3
+    from tkinter import *   ## notice lowercase 't' in tkinter here
+    import tkinter as tk
+import sqlite3
+import tkinter.messagebox
+
+# exec(open("./appointment.py").read())
